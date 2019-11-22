a284d9558c874dc927e24fcbbb1cb70fd65d1cd0
Wojciech Pęciak <35200310+wagon15@users.noreply.github.com>
2019-11-22 11:36:35 +0100

    Defining layout for desktop - landing and navbar

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
 // Toggle nav dropdown on button click
 document.getElementById("nav-dropdown").onclick = function() {
-    document.getElementById("linkList").classList.toggle("nav-bar__links--hidden");
+    document.getElementById("linkList").classList.toggle("nav-bar__links--is-open");
 };
 
 // Toggle nav dorpdown on outside button click
 window.onclick = function(event) {
     if (!event.target.matches('#nav-dropdown')) {
-    document.getElementById("linkList").classList.add("nav-bar__links--hidden");
+    document.getElementById("linkList").classList.remove("nav-bar__links--is-open");
 
     }
 }
+ // Hide scroll icon
+ const scrollIcon = document.getElementById("scrollIcon");
+ const className = "landing-section__scroll-icon--is-hidden";
+ document.onscroll = function() {
+    window.pageYOffset === 0 ? setTimeout( function() {scrollIcon.classList.remove(className);}, 2000) : scrollIcon.classList.add(className);
+ };
+
+ // Scrolling to sections
+ document.getElementById("aboutLink").onclick = function(event) {
+    event.preventDefault(); 
+    document.getElementById("about").scrollIntoView({ behavior: "smooth", block: "start"});
+ };
+ document.getElementById("projectsLink").onclick = function(event) {
+    event.preventDefault(); 
+    document.getElementById("projects").scrollIntoView({ behavior: "smooth", block: "start"});
+ };
+ document.getElementById("contactLink").onclick = function(event) {
+    event.preventDefault(); 
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth", block: "start"});
+ };
